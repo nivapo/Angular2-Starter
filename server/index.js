bf17384ebd9cc9927af9127d1b2c31cb4c2572fd
Gopi Krishna <www.krrishh.org@gmail.com>
2017-09-25 18:53:54 +0530

    JWT decoding token works

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
 var passport = require('passport');
+var JwtStrategy = require('passport-jwt').Strategy,
+  ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {
+  // jwtFromRequest: ExtractJwt.fromExtractors([function(req) {
+  //   return req.headers.authorization.split(' ')[1];    //This worked
+  // }]),
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'qwertyuiopasdfghjklzxcvbnm123456',
+  issuer: 'localhost:9000',
+  algorithms: ["HS256"],
+  passReqToCallback: true,
+  audience: 'localhost:9000'
+}
+
 app.use(passport.initialize());
 app.use(passport.session());
 // used to serialize the user for the session
@@ -16,10 +30,13 @@ passport.deserializeUser(function(id, done) {
     done(err, user);
   });
 });
-
+passport.use(new JwtStrategy(opts, function(req, jwt_payload, done) {
+  console.log(jwt_payload);
+  done(null, jwt_payload);
+}));
 var port = 9000;
 
-app.get('/', function(req, res) {
+app.get('/', passport.authenticate('jwt', { session: false }), function(req, res) {
   res.send(req.user || "No Data");
 });
 //Register the login service
